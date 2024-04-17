39db130d08ad2f0c73e0fa25a10015ea80dd19b1
Neil Chen <neilchen1998@gmail.com>
2024-04-17 18:21:10 -0500

    incorporated position info in hash_range function

--- include/math/mathlib.hpp
+++ include/math/mathlib.hpp
@@ -4,7 +4,7 @@
 #include <cstdlib>  // std::size_t
 #include <span> // std::span
 #include <functional>   // std::hash
-#include <numeric>   // std::accumulate
+#include <numeric>   // std::accumulate, std::reduce
 #include <concepts> // std::integral
 
 template <std::integral T>
@@ -13,12 +13,15 @@ std::size_t hash_range(std::span<T> vec)
     // creates a hasher
     std::hash<T> h;
 
+    // the position of the element
+    std::size_t pos = 0;
+
     // iterates through the entire span of the vector, calculates the hash value
     // and returns the value
-    return std::accumulate(vec.begin(), vec.end(), vec.size(),
+    return std::reduce(vec.begin(), vec.end(), vec.size(),  // std::reduce in theory runs faster than std::accumulate
         [&](std::size_t s, T v)
         {
-            return s ^= h(v) + 0x9e3779b9 + (s << 6) + (s >> 2);
+            return s ^= h(v) + 0x9e3779b9 + (s << 6) + (s >> 2) + (++pos << 7);
         });
 }
 
