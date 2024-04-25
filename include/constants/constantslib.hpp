1724592a52c469895075853e27b0f44d3966d0c3
Neil Chen <neilchen1998@gmail.com>
2024-04-25 07:24:23 -0500

    added comments for class constants

--- include/constants/constantslib.hpp
+++ include/constants/constantslib.hpp
@@ -5,14 +5,31 @@
 
 namespace constants
 {
+    /// @brief the empty piece
     inline constexpr int EMPTY { 0xFF };
+
+    /// @brief the grid size of the 8 puzzle problem
     inline constexpr int EIGHT_PUZZLE_SIZE { 3 };
+
+    /// @brief the grid size of the 8 puzzle problem
     inline constexpr int FIFTEEN_PUZZLE_SIZE { 4 };
+
+    /// @brief the total number of pieces of 8 puzzle problem
     inline constexpr int EIGHT_PUZZLE_NUM { (EIGHT_PUZZLE_SIZE * EIGHT_PUZZLE_SIZE) };
+
+    /// @brief the total number of pieces of 8 puzzle problem
     inline constexpr int FIFTEEN_PUZZLE_NUM { (FIFTEEN_PUZZLE_SIZE * FIFTEEN_PUZZLE_SIZE) };
+
+    /// @brief the right direction
     inline constexpr int RIGHT { 0 };
+
+    /// @brief the up direction
     inline constexpr int UP   { 1 };
+
+    /// @brief the left direction
     inline constexpr int LEFT  { 2 };
+
+    /// @brief the down direction
     inline constexpr int DOWN  { 3 };
 } // namespace constants
 
