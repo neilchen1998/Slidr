257c6be988dc85210c0334752b920a227928f0d4
Neil Chen <neilchen1998@gmail.com>
2024-05-02 19:41:36 -0500

    simplified func hash_range logic

--- include/math/mathlib.hpp
+++ include/math/mathlib.hpp
@@ -3,25 +3,18 @@
 
 #include <cstdlib>  // std::size_t
 #include <span> // std::span
-#include <functional>   // std::hash
 #include <numeric>   // std::accumulate, std::reduce
 #include <concepts> // std::integral
 
 template <std::integral T>
 std::size_t hash_range(std::span<T> vec)
 {
-    // creates a hasher
-    std::hash<T> h;
-
-    // the position of the element
-    std::size_t pos = 0;
-
     // iterates through the entire span of the vector, calculates the hash value
     // and returns the value
-    return std::reduce(vec.begin(), vec.end(), vec.size(),  // std::reduce in theory runs faster than std::accumulate
+    return std::accumulate(vec.begin(), vec.end(), vec.size(),  // std::reduce in theory runs faster than std::accumulate
         [&](std::size_t s, T v)
         {
-            return s ^= h(v) + 0x9e3779b9 + (s << 6) + (s >> 2) + (++pos << 7);
+            return s ^= v + 0x9e3779b9 + (s << 6) + (s >> 2);
         });
 }
 
