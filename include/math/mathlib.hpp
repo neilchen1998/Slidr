6d4463e36c5b4f6115a16ffa52bc68ba99ddba1b
Neil Chen <neilchen1998@gmail.com>
2024-05-02 19:44:17 -0500

    replaced with std::reduce in  func hash_range logic

--- include/math/mathlib.hpp
+++ include/math/mathlib.hpp
@@ -11,7 +11,7 @@ std::size_t hash_range(std::span<T> vec)
 {
     // iterates through the entire span of the vector, calculates the hash value
     // and returns the value
-    return std::accumulate(vec.begin(), vec.end(), vec.size(),  // std::reduce in theory runs faster than std::accumulate
+    return std::reduce(vec.begin(), vec.end(), vec.size(),  // std::reduce in theory runs faster than std::accumulate
         [&](std::size_t s, T v)
         {
             return s ^= v + 0x9e3779b9 + (s << 6) + (s >> 2);
