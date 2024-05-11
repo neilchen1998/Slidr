54be18c3e1b4a7d3c0b7ec841989e228f029996a
Neil Chen <neilchen1998@gmail.com>
2024-05-11 09:13:53 -0500

    added GeneratePatternsForFifteenPuzzleProblem func

--- apps/app.cc
+++ apps/app.cc
@@ -6,90 +6,223 @@
 #include <queue>    // std::priority_queue
 #include <tuple>    // std::tuple
 #include <ranges>    // std::ranges
+#include <random>   // std::random_device, std::mt19937, std::uniform_int_distribution
 
 #include "constants/constantslib.hpp"
 #include "math/mathlib.hpp"
+#include "pattern/patternlib.hpp"
 #include "node/nodelib.hpp"
 #include "solver/solverlib.hpp"
 
+const std::vector<int> FIFTEEN_GOAL_STATE {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, constants::EMPTY};
+
+template <int GridSize>
+struct Tester : Node<GridSize>
+{
+    std::vector<int> GetState() const { return this->state; }
+
+    int GetPosX() const { return this->posX; }
+};
+
+void SolveEightPuzzleProblem()
+{
+    // the 8 puzzle problem
+    std::cout << "=== 8 Puzzle Problem ===\n";
+    std::vector<int> state {1, 2, constants::EMPTY, 4, 5, 3, 7, 8, 6};
+    Node<constants::EIGHT_PUZZLE_SIZE> n(state);
+    Solver<constants::EIGHT_PUZZLE_SIZE> s = Solver(n);
+
+    auto [isSolved, iterations] = s.SolvePuzzle();
+
+    if (isSolved)
+    {
+        std::cout << "Solution found! Took " << iterations << " iteration(s)\n";
+    }
+    else
+    {
+        std::cout << "No solution was found. The given puzzle have odd number of inversions.\n";
+    }
+
+    auto solution = s.GetSolution();
+
+    std::cout << "Start:\n";
+    n.Print();
+    std::cout << "*****\n";
+
+    // only show the solution is the puzzle was solved
+    if (isSolved)
+    {
+        int i = 1;
+        for (const auto& sol : solution | std::views::drop(1))  // drop the start state
+        {
+            std::cout << "Step " << i << ":\n";
+            sol.Print();
+            std::cout << "*****\n";
+
+            ++i;
+        }
+    }
+}
+
+void SolveFifteenPuzzleProblem(const std::vector<int>& initialState, const std::string filename = "/home/neil_poseidon/C++/8-Puzzle/fifteen-puzzle-patterns.txt", bool write = false)
+{
+    // the 15 puzzle problem
+    std::cout << "=== 15 Puzzle Problem ===\n";
+    Node<constants::FIFTEEN_PUZZLE_SIZE> n(initialState);
+    Solver<constants::FIFTEEN_PUZZLE_SIZE> s = Solver(n);
+
+    std::cout << "Hash: " << n.GetHashValue() << "\n";
+
+    auto [isSolved, iterations] = s.SolvePuzzle();
+    auto solution = s.GetSolution();
+
+    if (isSolved)
+    {
+        std::cout << "Solution found! Took " << iterations << " iteration(s)\n";
+        std::cout << "Depth: " << s.GetDepth() << std::endl;
+    }
+    else
+    {
+        std::cout << "No solution was found. The given puzzle have odd number of inversions.\n";
+    }
+
+    std::cout << "Start:\n";
+    n.Print();
+    std::cout << "************\n";
+
+    // only show the solution is the puzzle was solved
+    if (isSolved)
+    {
+        // int i = 1;
+        // for (const auto& sol : solution | std::views::drop(1))  // drop the start state
+        // {
+        //     std::cout << "Step " << i << ":\n";
+        //     sol.Print();
+        //     std::cout << "************\n";
+
+        //     ++i;
+        // }
+
+        std::cout << "Move sequence:\n";
+        std::vector<int> sequence {s.GetSequence()};
+        if (write)
+        {
+            pattern::ExportSolution(n.GetHashValue(), sequence, filename);
+        }
+        int cnt = 1;
+        for (const auto& seq : sequence)
+        {
+            std::cout << seq;
+            if (cnt % 5 == 0)
+            {
+                std::cout << "\n";
+            }
+
+            ++cnt;
+        }
+        std::cout << "\n";
+    }
+}
+
+void GeneratePatternsForFifteenPuzzleProblem(int low, int high, const std::string filename = "/home/neil_poseidon/C++/8-Puzzle/fifteen-puzzle-patterns.txt")
+{
+    // auto generating patterns
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(low, high);
+    const int N = distrib(gen);
+
+    Node<constants::FIFTEEN_PUZZLE_SIZE> cur = FIFTEEN_GOAL_STATE;
+    int i = 0;
+    while (i < N)
+    {
+        std::vector<int> ava = cur.AvailableMoves();
+        std::uniform_int_distribution<> distrib(0, ava.size() - 1);
+        auto [state, posX] = cur.GetNextState(ava[distrib(gen)]);
+        cur = Node<constants::FIFTEEN_PUZZLE_SIZE>(state, posX, i);
+        ++i;
+    }
+    
+    std::cout << "Result (" << N << "):\n";
+    cur.Print();
+    Solver<constants::FIFTEEN_PUZZLE_SIZE> s = Solver(cur);
+
+    auto [isSolved, iterations] = s.SolvePuzzleWithPatterns(filename);
+
+    if (isSolved)
+    {
+        std::cout << "Actual steps: " << s.GetDepth() << std::endl;
+        std::vector<int> sequence = s.GetSequence();
+        pattern::ExportSolution(cur.GetHashValue(), sequence, filename);
+    }
+}
+
+void SolveFifteenPuzzleProblemsWithPattern(const std::vector<int>& initialState, const std::string filename = "/home/neil_poseidon/C++/8-Puzzle/fifteen-puzzle-patterns.txt", bool write = false)
+{
+    // the 15 puzzle problem
+    std::cout << "=== 15 Puzzle Problem (With Pattern) ===\n";
+    Node<constants::FIFTEEN_PUZZLE_SIZE> n(initialState);
+    Solver<constants::FIFTEEN_PUZZLE_SIZE> s = Solver(n);
+
+    std::cout << "Hash: " << n.GetHashValue() << "\n";
+
+    auto [isSolved, iterations] = s.SolvePuzzleWithPatterns(filename);
+    auto solution = s.GetSolution();
+
+    if (isSolved)
+    {
+        std::cout << "Solution found! Took " << iterations << " iteration(s)\n";
+    }
+    else
+    {
+        std::cout << "No solution was found. The given puzzle have odd number of inversions.\n";
+    }
+
+    std::cout << "Start:\n";
+    n.Print();
+    std::cout << "************\n";
+
+    // only show the solution is the puzzle was solved
+    if (isSolved)
+    {
+        int i = 1;
+        for (const auto& sol : solution | std::views::drop(1))  // drop the start state
+        {
+            std::cout << "Step " << i << ":\n";
+            sol.Print();
+            std::cout << "************\n";
+
+            ++i;
+        }
+
+        std::cout << "Move sequence:\n";
+        std::vector<int> sequence = s.GetSequence();
+        if (write)
+        {
+            pattern::ExportSolution(n.GetHashValue(), sequence, filename);
+        }
+        int cnt = 1;
+        for (const auto& seq : sequence)
+        {
+            std::cout << seq;
+            if (cnt % 5 == 0)
+            {
+                std::cout << "\n";
+            }
+
+            ++cnt;
+        }
+        std::cout << "\n";
+    }
+}
+
+const std::string filename("/home/neil_poseidon/C++/8-Puzzle/tmp.txt");
+
 int main(int argc, char* argv[])
 {
-    {
-        // the 8 puzzle problem
-        std::cout << "=== 8 Puzzle Problem ===\n";
-        std::vector<int> state {1, 2, constants::EMPTY, 4, 5, 3, 7, 8, 6};
-        Node<constants::EIGHT_PUZZLE_SIZE> n(state);
-        Solver<constants::EIGHT_PUZZLE_SIZE> s = Solver(n);
-
-        auto [isSolved, iterations] = s.SolvePuzzle();
-
-        if (isSolved)
-        {
-            std::cout << "Solution found! Took " << iterations << " iteration(s)\n";
-        }
-        else
-        {
-            std::cout << "No solution was found. The given puzzle have odd number of inversions.\n";
-        }
-
-        auto solution = s.GetSolution();
-
-        std::cout << "Start:\n";
-        n.Print();
-        std::cout << "*****\n";
-
-        // only show the solution is the puzzle was solved
-        if (isSolved)
-        {
-            int i = 1;
-            for (const auto& sol : solution | std::views::drop(1))  // drop the start state
-            {
-                std::cout << "Step " << i << ":\n";
-                sol.Print();
-                std::cout << "*****\n";
-
-                ++i;
-            }
-        }
-    }
-
-    {
-        // the 15 puzzle problem
-        std::cout << "=== 15 Puzzle Problem ===\n";
-        std::vector<int> initialState {10, 13, 5, 4, 6, constants::EMPTY, 15, 14, 3, 11, 9, 8, 7, 1, 12, 2};
-        Node<constants::FIFTEEN_PUZZLE_SIZE> n(initialState);
-        Solver<constants::FIFTEEN_PUZZLE_SIZE> s = Solver(n);
-
-        auto [isSolved, iterations] = s.SolvePuzzle();
-        auto solution = s.GetSolution();
-
-        if (isSolved)
-        {
-            std::cout << "Solution found! Took " << iterations << " iteration(s)\n";
-        }
-        else
-        {
-            std::cout << "No solution was found. The given puzzle have odd number of inversions.\n";
-        }
-
-        std::cout << "Start:\n";
-        n.Print();
-        std::cout << "************\n";
-
-        // only show the solution is the puzzle was solved
-        if (isSolved)
-        {
-            int i = 1;
-            for (const auto& sol : solution | std::views::drop(1))  // drop the start state
-            {
-                std::cout << "Step " << i << ":\n";
-                sol.Print();
-                std::cout << "************\n";
-
-                ++i;
-            }
-        }
-    }
+    // SolveFifteenPuzzleProblem({1, 2, 4, 12, 5, 3, 8, 7, 9, 10, 11, constants::EMPTY, 14, 6, 15, 13}, filename, true);
+    GeneratePatternsForFifteenPuzzleProblem(400, 600, filename);
+    // SolveFifteenPuzzleProblemsWithPattern({1, 2, 12, 15, 3, 9, 7, 4, 10, 5, constants::EMPTY, 8, 14, 6, 11, 13});   // munmap_chunk(): invalid pointer
 
     return EXIT_SUCCESS;
 }
