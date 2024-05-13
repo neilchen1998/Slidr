51d3960ac7c2697aa7f6ac347591e7dbfb6821e1
Neil Chen <neilchen1998@gmail.com>
2024-05-13 12:12:49 -0500

    changed hash_range func to FNV-1a

--- include/math/mathlib.hpp
+++ include/math/mathlib.hpp
@@ -9,13 +9,17 @@
 template <std::integral T>
 std::size_t hash_range(std::span<T> vec)
 {
-    // iterates through the entire span of the vector, calculates the hash value
-    // and returns the value
-    return std::reduce(vec.begin(), vec.end(), vec.size(),  // std::reduce in theory runs faster than std::accumulate
-        [&](std::size_t s, T v)
-        {
-            return s ^= v + 0x9e3779b9 + (s << 6) + (s >> 2);
-        });
+    // uses FNV-1a hash algorithm
+    // FNV offset basis: 0xcbf29ce484222325
+    // FNV prime: 0x100000001b3
+    std::size_t hash = 0xcbf29ce484222325;
+    for (const T& v : vec)
+    {
+        hash ^= v;
+        hash *= 0x100000001b3;
+    }
+
+    return hash;
 }
 
 #endif // INCLUDE_MATH_MATHLIB_H_
