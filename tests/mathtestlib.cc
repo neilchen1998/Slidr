8d98fc2fcf355844e5f436e36259c06ec5215889
Neil Chen <neilchen1998@gmail.com>
2025-06-06 19:28:49 -0500

    removed nonsensical hash_range func test case

--- tests/mathtestlib.cc
+++ tests/mathtestlib.cc
@@ -27,28 +27,3 @@ TEST_CASE( "Hash Range Function", "[main]" )
         REQUIRE (hashV2 == hashV3);
     }
 }
-
-TEST_CASE( "Properties of the Hash Range Function", "[main]" )
-{
-    SECTION ( "Layout 1", "[main]")
-    {
-        std::vector<int> v1 {3, 1, 4, 2, 8, 7, 6, 5, constants::EMPTY};
-        std::vector<int> v2 {3, 1, constants::EMPTY, 2, 8, 7, 6, 5, 4};
-
-        std::size_t hashV1 = hash_range(std::span(v1.begin(), v1.end()));
-        std::size_t hashV2 = hash_range(std::span(v2.begin(), v2.end()));
-
-        REQUIRE (hashV2 < hashV1);
-    }
-
-    SECTION ( "Layout 2", "[main]")
-    {
-        std::vector<int> v1 {2, 6, 5, 8, 1, 4, constants::EMPTY, 7, 3};
-        std::vector<int> v2 {2, 6, 5, 8, 1, 3, constants::EMPTY, 7, 4};
-
-        std::size_t hashV1 = hash_range(std::span(v1.begin(), v1.end()));
-        std::size_t hashV2 = hash_range(std::span(v2.begin(), v2.end()));
-
-        REQUIRE (hashV2 > hashV1);
-    }
-}
