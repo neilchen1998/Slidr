0e57d25d5789f7e86211dc9596cf02d18f330f7e
Neil Chen <neilchen1998@gmail.com>
2025-06-09 23:04:07 -0500

    added compiler flags & boost lib

--- compile_and_test.sh
+++ compile_and_test.sh
@@ -1,1 +1,10 @@
-cmake --build build && cmake --build build --target test
+#!/bin/bash
+
+# configures the repo
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Test &&
+
+# builds the repo
+cmake --build build &&
+
+# runs all the tests
+cmake --build build --target test
