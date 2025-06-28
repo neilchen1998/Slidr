8a16f93809870113304afc579ab65ae693a26599
Neil <neilchen1998@gmail.com>
2025-06-28 15:03:31 -0500

    created blank hpp & cc files for buck queue

---
+++ include/container/bucketqueuelib.hpp
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_CONTAINER_BUCKETQUEUE_H_
+#define INCLUDE_CONTAINER_BUCKETQUEUE_H_
+
+#include <vector>   // std::vector
+#include <cstddef>  // std::size_t
+#include <tuple>    // std::tuple
+#include <memory>   // std::shared_ptr
+
+class BuckQueue
+{
+public:
+
+private:
+
+};
+
+#endif // INCLUDE_CONTAINER_BUCKETQUEUE_H_
