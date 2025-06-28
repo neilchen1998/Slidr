bfffaa70c8f0859868e3ac04fffdbdf134453b2d
Neil <neilchen1998@gmail.com>
2025-06-28 15:20:09 -0500

    increase the # of problems

--- gprof/solverbenchmark.cc
+++ gprof/solverbenchmark.cc
@@ -12,15 +12,32 @@
 
 int main(int argc, char* argv[])
 {
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; ++i)
     {
-        std::vector<int> layout {1, 7, constants::EMPTY, 8, 4, 3, 5, 2, 6};
-        Solver s = Solver(layout);
-        auto [isSolved, totalIters] = s.SolvePuzzle();
+        {
+            std::vector<int> layout {1, 7, constants::EMPTY, 8, 4, 3, 5, 2, 6};
+            Solver s = Solver(layout);
+            auto [isSolved, totalIters] = s.SolvePuzzle();
+        }
 
-        fmt::print("# of iterations: {}\tTotal moves: {}\n", totalIters, s.GetNumOfMoves());
-        s.PrintPath();
+        {
+            std::vector<int> layout {6, 3, 8, 2, 1, 7, constants::EMPTY, 5, 4};
+            Solver s = Solver(layout);
+            auto [isSolved, totalIters] = s.SolvePuzzle();
+        }
 
-        fmt::print("Moves: {}\n", s.GetSolution());
+        {
+            std::vector<int> layout {7, 4, 1, 5, 2, 3, 8, 6, constants::EMPTY};
+            Solver s = Solver(layout);
+            auto [isSolved, totalIters] = s.SolvePuzzle();
+        }
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    fmt::print("Total time: {} ms\n", duration.count());
+
     return EXIT_SUCCESS;
 }
