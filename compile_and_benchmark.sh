c7871c68ebfb826c546634805566d4579f90fe06
Neil <neilchen1998@gmail.com>
2025-06-28 08:27:45 -0500

    renamed benchmark files

--- compile_and_benchmark.sh
+++ compile_and_benchmark.sh
@@ -7,5 +7,5 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Benchmark &&
 cmake --build build &&
 
 # runs the benchmark
-./build/benchmarks/mathbenchmarklib &&
-./build/benchmarks/nodebenchmarklib
+./build/benchmarks/mathbenchmark &&
+./build/benchmarks/nodebenchmark
