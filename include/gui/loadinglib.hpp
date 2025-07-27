0d84cffd4347a0568af539639182a13f6f5f1dd9
Neil Chen <neilchen1998@gmail.com>
2025-07-27 12:57:37 -0500

    created LoadingAnimation class

---
+++ include/gui/loadinglib.hpp
@@ -0,0 +1,76 @@
+#ifndef INCLUDE_GUI_LOADINGLIB_H_
+#define INCLUDE_GUI_LOADINGLIB_H_
+
+#include "raylib.h"
+
+/// @brief The states of the game
+enum struct LoadingState : int
+{
+    SMALL_BOX_BLINKING = 0,
+    LEFT_BOX_GROWING,
+    RIGHT_BOX_GROWING,
+    LETTER_APPEARING,
+    ENDING
+};
+
+class LoadingAnimation
+{
+public:
+    LoadingAnimation(int screenWidth, int screenHeight)
+        : curState_(LoadingState::SMALL_BOX_BLINKING),
+        screenWidth_(screenWidth),
+        screenHeight_(screenHeight),
+        logoPositionX_(screenWidth/2 - 128),
+        logoPositionY_(screenHeight/2 - 128),
+        leftSideRecHeight_(16),
+        topSideRecWidth_(16),
+        bottomSideRecWidth_(16),
+        rightSideRecHeight_(16),
+        lettersCount_(0),
+        framesCounter_(0),
+        alpha_(1.0f),
+        totalCnt_(0)
+    {
+    }
+
+    ~LoadingAnimation() = default;
+
+    /// @brief Update the state
+    void Update();
+
+    /// @brief Draw the the state on the screen
+    void Draw();
+
+private:
+    /// @brief The current state of the game
+    LoadingState curState_;
+
+    /// @brief The width of the main screen
+    int screenWidth_;
+
+    /// @brief The height of the main screen
+    int screenHeight_;
+
+    int leftSideRecHeight_;
+
+    int topSideRecWidth_;
+
+    int lettersCount_;
+
+    float alpha_;
+
+    const int logoPositionX_;
+
+    const int logoPositionY_;
+
+    /// @brief 
+    int bottomSideRecWidth_;
+
+    int rightSideRecHeight_;
+
+    unsigned long framesCounter_;
+
+    unsigned totalCnt_;
+};
+
+#endif // INCLUDE_GUI_LOADINGLIB_H_
