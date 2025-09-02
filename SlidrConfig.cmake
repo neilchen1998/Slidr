f89169e39d5c1c3f6d96703606013dfb27ccf3fd
Neil Chen <neilchen1998@gmail.com>
2025-09-01 20:54:53 -0500

    commented out SlidrConfig

--- SlidrConfig.cmake
+++ SlidrConfig.cmake
@@ -1,11 +1,12 @@
-install(EXPORT SlidrTargets
-    FILE SlidrTargets.cmake
-    NAMESPACE Slidr::
-    DESTINATION lib/cmake/Slidr
-)
+# Tell CMake to export a set of targets for find_package(Slidr)
+# install(EXPORT SlidrTargets
+#     FILE SlidrTargets.cmake
+#     NAMESPACE Slidr::
+#     DESTINATION lib/cmake/Slidr
+# )
 
-configure_package_config_file(
-    ${CMAKE_CURRENT_SOURCE_DIR}/SlidrConfig.cmake
-    ${CMAKE_CURRENT_BINARY_DIR}/SlidrConfig.cmake
-    INSTALL_DESTINATION lib/cmake/Slidr
-)
+# configure_package_config_file(
+#     ${CMAKE_CURRENT_SOURCE_DIR}/SlidrConfig.cmake
+#     ${CMAKE_CURRENT_BINARY_DIR}/SlidrConfig.cmake
+#     INSTALL_DESTINATION lib/cmake/Slidr
+# )
